fce429c6a777e25f94912e638d2c9874b5638277
Lsimpson-7loopintermodal <lsimpson@7loopintermodal.com>
2026-04-19 14:29:07 -0500

    Create servicesMix.js

---
+++ servicesMix.js
@@ -0,0 +1,9 @@
+import { C } from "../theme"
+
+export const servicesMix = [
+  { name: "Intermodal Drayage", value: 42, color: C.accent },
+  { name: "Regional TL", value: 24, color: C.green },
+  { name: "OTR Flatbed/DV", value: 18, color: C.gold },
+  { name: "Power-Only", value: 10, color: C.branden },
+  { name: "Oversize/Open Deck", value: 6, color: C.red },
+]
