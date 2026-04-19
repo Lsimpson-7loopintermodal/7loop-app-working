5a3c1c2f489984f4c7c79bb55451f1766ed88aba
Lsimpson-7loopintermodal <lsimpson@7loopintermodal.com>
2026-04-19 14:28:18 -0500

    Create brokerVsDirect.js

---
+++ brokerVsDirect.js
@@ -0,0 +1,10 @@
+export const brokerVsDirect = [
+  { w: "Wk 1", broker: 71, direct: 29 },
+  { w: "Wk 2", broker: 68, direct: 32 },
+  { w: "Wk 3", broker: 66, direct: 34 },
+  { w: "Wk 4", broker: 65, direct: 35 },
+  { w: "Wk 5", broker: 63, direct: 37 },
+  { w: "Wk 6", broker: 60, direct: 40 },
+  { w: "Wk 7", broker: 57, direct: 43 },
+  { w: "Wk 8", broker: 54, direct: 46 },
+]
