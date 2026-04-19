97265ee1dc20e7d9982fe4315385cb805109aae3
Lsimpson-7loopintermodal <lsimpson@7loopintermodal.com>
2026-04-19 14:23:39 -0500

    Create theme.js

---
+++ theme.js
@@ -0,0 +1,23 @@
+export const C = {
+  bg: "#060C14",
+  s1: "#08111C",
+  s2: "#0B1724",
+  card: "#0D1A28",
+  border: "#152233",
+  text: "#DDE9F5",
+  muted: "#406080",
+  faint: "#0F1E30",
+
+  accent: "#0E7FC4",
+  green: "#17A86A",
+  gold: "#CC8F28",
+  red: "#C03535",
+  amber: "#CC8F28",
+
+  shannon: "#0E9E8A",
+  branden: "#1A7FD4",
+  larry: "#C47E1A",
+  aaralyn: "#8B5CF6",
+  jonathan: "#E05C2A",
+  kelsey: "#D4396A",
+}
