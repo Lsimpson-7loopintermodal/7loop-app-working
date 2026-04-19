ec7b775a80e9bf31176683ca383c74a3e31c82ae
Lsimpson-7loopintermodal <lsimpson@7loopintermodal.com>
2026-04-19 14:27:44 -0500

    Create outreach.js

---
+++ outreach.js
@@ -0,0 +1,10 @@
+export const outreachData = [
+  { w: "Wk 1", sent: 42, opened: 18, replied: 4 },
+  { w: "Wk 2", sent: 56, opened: 24, replied: 6 },
+  { w: "Wk 3", sent: 48, opened: 21, replied: 5 },
+  { w: "Wk 4", sent: 67, opened: 31, replied: 8 },
+  { w: "Wk 5", sent: 71, opened: 34, replied: 9 },
+  { w: "Wk 6", sent: 63, opened: 29, replied: 7 },
+  { w: "Wk 7", sent: 82, opened: 40, replied: 11 },
+  { w: "Wk 8", sent: 94, opened: 48, replied: 14 },
+]
